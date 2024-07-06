02ccd95eb73002c071ca39a8176d3c86fc220b9f
Sergey <seygorin@gmail.com>
2024-07-06 17:42:20 +0500

    feat: create base stucture for the task

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', 'react-hooks', 'react-compiler', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
